895b0aba2ab10071f85bc3b240e2849622604ac3
Ígor Saádeh <igor.saadeh@gmail.com>
2024-04-14 19:52:11 -0300

    Transição de Quadros

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveCamera : MonoBehaviour
+{
+    private Camera mainCamera;
+    //public Transform targetPosition; // O destino para onde você deseja mover a câmera
+    public float moveTime = 1f; // O tempo que levará para a câmera se mover para o destino
+    Vector3 startPosition;
+
+    private void Awake()
+    {
+        foreach (GameObject rootObject in SceneManager.GetActiveScene().GetRootGameObjects())
+        {
+            Camera cameraComponent = rootObject.GetComponent<Camera>();
+            if (cameraComponent != null)
+            {
+                mainCamera = cameraComponent;
+                break;
+            }
+        }
+
+        if (mainCamera != null)
+        {
+            Debug.Log("Referência da câmera encontrada: " + mainCamera.name);
+        }
+        else
+        {
+            Debug.LogError("Nenhuma câmera encontrada na cena.");
+        }
+    }
+
+    private void Start()
+    {
+        startPosition = mainCamera.transform.position;  //0,0,0
+    }
+
+    public void MoveCameraToTarget()
+    {
+        Vector3 newPosition = new Vector3(startPosition.x + 5.5f, startPosition.y, startPosition.z);
+
+        StartCoroutine(MoveToPosition(newPosition, moveTime));
+    }
+
+    IEnumerator MoveToPosition(Vector3 targetPosition, float timeToMove)
+    {
+        float elapsedTime = 0;
+        //Vector3 startingPos = transform.position;
+
+        while (elapsedTime < timeToMove)
+        {
+            mainCamera.transform.position = Vector3.Lerp(startPosition, targetPosition, (elapsedTime / timeToMove));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        mainCamera.transform.position = targetPosition;
+    }
+}
