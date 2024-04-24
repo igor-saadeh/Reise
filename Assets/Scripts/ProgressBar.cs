a83b1ee86dc205240c93f83075500caea919baf0
Ígor Saádeh <igor.saadeh@gmail.com>
2024-04-23 23:51:38 -0300

    Minigame 3 - Ajustes no código - Uso de Eventos

--- Assets/Scripts/ProgressBar.cs
+++ Assets/Scripts/ProgressBar.cs
@@ -2,31 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class ProgressBar : MonoBehaviour
 {
     public static ProgressBar Instance { get; private set; }
-    public Slider slider;
+    //public UnityEngine.UI.Slider slider;
 
     private void Awake()
     {
         Instance = this;
-        slider.gameObject.SetActive(false);
+        //gameObject.SetActive(false);
     }
 
     public void ShowProgressBar()
     {
-        slider.gameObject.SetActive(true);
-        slider.value = 0f; // Reinicia o valor da barra para zero
+        //slider.gameObject.SetActive(true);
+        gameObject.SetActive(true);
+        //slider.value = 0f; // Reinicia o valor da barra para zero
+        GetComponent<UnityEngine.UI.Slider>().value = 0f;
     }
 
     public void HideProgressBar()
     {
-        slider.gameObject.SetActive(false);
+        gameObject.SetActive(false);
     }
 
     public void SetProgress(float progress)
     {
-        slider.value = progress; // Define o valor da barra de acordo com o progresso recebido
+        GetComponent<UnityEngine.UI.Slider>().value = progress; // Define o valor da barra de acordo com o progresso recebido
     }
 }
--- Assets/Scripts/WateringCan.cs
+++ Assets/Scripts/WateringCan.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Timeline;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class WateringCan : MonoBehaviour
 {
+    private bool isDragging = false;
+    private Vector3 offset;
+
     private void OnTriggerEnter(Collider other)
     {
         IWaterable waterableObject = other.GetComponent<IWaterable>();
@@ -21,5 +25,32 @@ public class WateringCan : MonoBehaviour
         {
             waterableObject.StopWatering();
         }
+    }
+
+    void OnMouseDown()
+    {
+        isDragging = true;
+        offset = transform.position - GetMouseWorldPosition();
+    }
+
+    void OnMouseUp()
+    {
+        isDragging = false;
+    }
+
+    void Update()
+    {
+        if (isDragging)
+        {
+            Vector3 newPosition = GetMouseWorldPosition() + offset;
+            transform.position = newPosition;
+        }
+    }
+
+    private Vector3 GetMouseWorldPosition()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = -Camera.main.transform.position.z;
+        return Camera.main.ScreenToWorldPoint(mousePosition);
     }
 }
