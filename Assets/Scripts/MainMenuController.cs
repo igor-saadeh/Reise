b276c6322230b9320daac1b336cce80e49f69e48
Chimas <50328145+igor-saadeh@users.noreply.github.com>
2024-07-04 18:55:33 -0300

    Menu - Arte dos botões + Tela de Créditos

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -5,8 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+    //private GameObject CreditsMenu;
+
+    private void Start()
+    {
+        //CreditsMenu = GameObject.Find("CreditsMenu");
+        //Debug.Log($"{CreditsMenu.gameObject.name}");
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void CreditScreen()
+    {
+        Debug.Log("Teste");
+        //CreditsMenu.SetActive(true);
     }
 }
